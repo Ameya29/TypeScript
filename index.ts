5baae1721dc7f457d8dd3a3d6a3ddb26b6dd0566
Ameya29 <67052678+Ameya29@users.noreply.github.com>
2022-02-16 00:31:41 +0530

    Add files via upload

--- index.ts
+++ index.ts
@@ -1,17 +1,64 @@
-console.log("Hello world");
-const op =<HTMLInputElement> document.getElementById("op");
-const input =<HTMLInputElement> document.getElementById("text-box");
+import { question } from "readline-sync";
 
-document.querySelectorAll(".calc-btn").forEach(function (element: Element) {
-    element.addEventListener("click", function (): void {
-        if (input) {
-            input.value += element.innerHTML;
-        }
-    });
-});
+type Operator = '+' | '-' | '*' | '/'
 
-document.getElementById("equal")?.addEventListener("click", function (): void {
-    if (input) {
-        input.value = `${eval(input.value)}`;
+function main(): void
+{
+    const firstStr: string = question("Enter 1st Number: \n")
+    const operator: string = question("Enter Operation: \n")
+    const secondStr: string = question("Enter 2nd Number: \n")
+    
+    const validInp: boolean = isNumber(firstStr) && isOperator(operator) && isNumber(secondStr);
+    
+    if(validInp)
+    {
+        const firstNum: number = parseInt(firstStr)
+        const secondNum: number = parseInt(secondStr)
+        const result = calculate(firstNum, operator as Operator, secondNum);
+        console.log(result);
+
     }
-});
+    else
+    {
+        console.log("\nInvalid Input")
+        main();
+    }
+}
+
+function calculate(firstNum: number, operator: Operator, secondNum: number)
+{
+    switch(operator)
+    {
+        case '+':
+            return firstNum + secondNum;
+        case '-':
+            return firstNum - secondNum;
+        case '*':
+            return firstNum * secondNum;
+        case '/':
+            return firstNum / secondNum;
+    }
+}
+
+function isOperator(operator: string): boolean
+{
+    switch(operator)
+    {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            return true
+        default:
+            return false
+    }
+}
+
+function isNumber(str: string): boolean
+{
+    const maybeNum = parseInt(str);
+    const isNum: boolean = !isNaN(maybeNum);
+    return isNum;
+}
+
+main();
